23764d3a8d0ed565855530edf0b2742b6fd4aaeb
Pavel Kryvaltsevich <pavel_kh@protonmail.com>
2022-03-21 14:13:12 +0300

    Les5 With Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import "./App.css";
+import { Todolist } from "./Todolist";
+import { v1 } from "uuid"
+import { addTaskAC, changeStatusAC, removeTaskAC, tasksReducer } from "./store/tasksReducer";
+
+export type FilterValuesType = "all" | "active" | "completed";
 
 function App() {
+  let [tasks, dispatchTask] = useReducer(tasksReducer,[
+    { id: v1(), title: "HTML&CSS", isDone: true },
+    { id: v1(), title: "JS", isDone: true },
+    { id: v1(), title: "ReactJS", isDone: false },
+    { id: v1(), title: "Rest API", isDone: false },
+    { id: v1(), title: "GraphQL", isDone: false },
+  ]);
+  let [filter, setFilter] = useState<FilterValuesType>("all");
+
+  function removeTask(id: string) {
+        dispatchTask(removeTaskAC(id))
+  };
+
+  function addTask(title: string) {
+    dispatchTask(addTaskAC(title))
+  };
+
+  function changeStatus(taskId: string, isDone: boolean) {
+    dispatchTask(changeStatusAC(taskId, isDone))
+  }
+
+  let tasksForTodolist = tasks;
+
+  if (filter === "active") {
+    tasksForTodolist = tasks.filter((t) => t.isDone === false);
+  }
+  if (filter === "completed") {
+    tasksForTodolist = tasks.filter((t) => t.isDone === true);
+  }
+
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist
+        title="What to learn"
+        tasks={tasksForTodolist}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
+        addTask={addTask}
+        changeTaskStatus={changeStatus}
+        filter={filter}
+      />
     </div>
   );
 }
 
 export default App;
+function changeStatusType(taskId: string, isDone: boolean): import("./store/tasksReducer").mainType {
+  throw new Error("Function not implemented.");
+}
+
---
+++ src/store/tasksReducer.ts
@@ -0,0 +1,58 @@
+import { v1 } from "uuid";
+import { TaskType } from "../Todolist";
+
+export const tasksReducer = (state: Array<TaskType>, action: mainType) => {
+switch(action.type) {
+    case "REMOVE-TASK": {
+        let newState = state.filter(f=>f.id !== action.payload.id)
+return newState
+    }
+    case "ADD-TASK": {
+        let task = { id: v1(), title: action.payload.title, isDone: false }  ;
+        let newState = [task,...state] 
+        
+        return newState
+    }
+    case "CHANGE-STATUS": {
+        let newState = state.map(m=>m.id === action.payload.Id ? {...m, isDone: action.payload.isDone} : m)
+        return newState
+    }
+    default: return state
+}
+};
+
+export type mainType = removeTaskType | addTaskType | changeStatusType;
+
+type removeTaskType = ReturnType<typeof removeTaskAC>;
+export const removeTaskAC = (id: string) => {
+    return {
+        type: "REMOVE-TASK",
+        payload: {
+            id: id
+
+        }
+    } as const
+}
+
+type addTaskType = ReturnType<typeof addTaskAC>;
+export const addTaskAC = (title: string) => {
+    return {
+        type: "ADD-TASK",
+        payload: {
+            title:title
+
+        }
+    } as const
+}
+
+type changeStatusType = ReturnType<typeof changeStatusAC>;
+export const changeStatusAC = (taskId: string, isDone: boolean) => {
+    return {
+        type: "CHANGE-STATUS",
+        payload: {
+            Id: taskId,
+            isDone:isDone
+
+        }
+    } as const
+}
